e44299742274f830121710f25354010419443e1d
Nicolas DENIAUD <bitbucket@feldoe.net>
2024-03-25 17:51:25 +0100

    fix: increment counter with string as well

--- src/Contracts/ExportProgressCounter.php
+++ src/Contracts/ExportProgressCounter.php
@@ -6,9 +6,9 @@ namespace Atx\ExportProgress\Contracts;
 
 interface ExportProgressCounter
 {
-    public function increment(string $uuid, ?int $modelId = null): void;
+    public function increment(string $uuid, int|string|null $modelId = null): void;
 
-    public function getCounter(string $uuid, ?int $modelId = null): int;
+    public function getCounter(string $uuid, int|string|null $modelId = null): int;
 
-    public function clearCounter(string $uuid, ?int $modelId = null): void;
+    public function clearCounter(string $uuid, int|string|null $modelId = null): void;
 }
--- src/ExportProgressCounter.php
+++ src/ExportProgressCounter.php
@@ -9,28 +9,28 @@ use Illuminate\Support\Facades\Cache;
 
 final class ExportProgressCounter implements ExportProgressCounterContract
 {
-    public function increment(string $uuid, ?int $modelId = null): void
+    public function increment(string $uuid, int|string|null $modelId = null): void
     {
         $key = $this->getCacheKey($uuid, $modelId);
 
         Cache::increment($key);
     }
 
-    public function getCounter(string $uuid, ?int $modelId = null): int
+    public function getCounter(string $uuid, int|string|null $modelId = null): int
     {
         $key = $this->getCacheKey($uuid, $modelId);
 
         return (int) Cache::get($key, 0); // @phpstan-ignore-error
     }
 
-    public function clearCounter(string $uuid, ?int $modelId = null): void
+    public function clearCounter(string $uuid, int|string|null $modelId = null): void
     {
         $key = $this->getCacheKey($uuid, $modelId);
 
         Cache::forget($key);
     }
 
-    private function getCacheKey(string $uuid, ?int $modelId): string
+    private function getCacheKey(string $uuid, int|string|null $modelId): string
     {
         if ($modelId === null) {
             $modelId = 'no_model';
